edebe2d0445544f188d760803bdcc0bf46f72f7f
Aleksey Tiknonov <alekseytesla@Yandex.ru>
2021-12-06 22:21:51 +0400

    Update 2.py

--- Cristine/Lab4/2.py
+++ Cristine/Lab4/2.py
@@ -4,6 +4,8 @@ mass = []
 for i in range(n):
     mass.append(rnd.randint(-100, 100))
 summ=0
+proizvedeniye=1
 for i in range(4):
     summ=mass[i]+summ
+    proizvedeniye=mass[i]*proizvedeniye
 print(summ)
