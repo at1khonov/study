d6fa570956a2df43aed6206ccda45a7fbd97081f
Aleksey Tiknonov <alekseytesla@Yandex.ru>
2021-12-06 23:05:11 +0400

    123

--- Ignat/Lab6/2.py
+++ Ignat/Lab6/2.py
@@ -1,19 +1,29 @@
+from math import sqrt
 import random as rnd
 
-k = int(input("Введите количество столбцов: "))  # Задание пользователем количества столбцов
+
+def distance(x1, y1, x2, y2):
+    dist = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2))
+    return dist
+
+
 n = int(input("Введите количество строк: "))  # Задание пользователем количества строк
-k1 = int(input('Введите k1: '))
-k2 = int(input('Введите k2: '))
-mass = []  # Объявление пустого списка массива
+mass = []
 row = []  # Объявление пустого списка строки
 for i in range(n):  # Цикл для заполнения массива случайными значениям
-    for i in range(k):
+    for i in range(2):
         row.append(rnd.randint(-40, 30))
     mass.append(row)
     row = []
+
 for i in range(n):  # Цикл для вывода исходного массива
-    for j in range(k):
+    for j in range(2):
         print(mass[i][j], end=' ')
     print()
-
-
+max=0
+for i in range(n):
+    for j in range(n):
+        l=int(distance(mass[i][0], mass[i][1], mass[j][0], mass[i][1]))
+        if max < l:
+            max = l
+print(max)
